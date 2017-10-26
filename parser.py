48d1d0932308ee02c93321ebb7b8cab46351447c
T1kon <surinnik@gmail.com>
2017-10-26 22:01:11 +0300

    sad

---
+++ parser.py
@@ -0,0 +1,53 @@
+import html.parser
+import re
+import time
+
+from bs4 import BeautifulSoup
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+
+URL = r'http://ct.wunderbar.name/tpstud.php?tn=512'
+TAG_RE = re.compile(r'<[^>]+>')
+QUESTIONS_COUNT = 50
+OPTS = webdriver.ChromeOptions()
+OPTS.binary_location = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+
+
+def remove_tags(text):
+    return TAG_RE.sub('', str(text))
+
+
+def parsek(output_dict):
+
+    driver = webdriver.Chrome(chrome_options=OPTS)
+
+    driver.get(URL)
+    time.sleep(0.1)
+
+    elem = driver.find_element_by_name('exam')
+    elem.send_keys("521703471")
+    elem.send_keys(Keys.ENTER)
+    time.sleep(0.1)
+
+    driver.find_element_by_class_name('button').click()
+    time.sleep(0.1)
+
+    for i in range(QUESTIONS_COUNT):
+        source = driver.page_source
+        soup = BeautifulSoup(source, 'html.parser')
+        output_dict[remove_tags(soup.find('td', {'align': 'center'})).rstrip()] = [
+            remove_tags(text) for text in soup.find_all('label')]
+        time.sleep(0.1)
+        driver.find_element_by_class_name('button').click()
+    driver.quit()
+
+
+QUESTIONS = dict()
+# for i in range(10):
+parsek(QUESTIONS)
+
+with open('asd.txt', 'w') as file:
+    file.write(str(QUESTIONS))
+    file.close()
